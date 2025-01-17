710f75355936d3c229c8361cac4f503867cdffe8
artemsmirnov10 <chernyuk79@gmail.com>
2025-01-17 18:26:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Rainbow")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Artem")
+})
+basic.forever(function () {
+	
+})
